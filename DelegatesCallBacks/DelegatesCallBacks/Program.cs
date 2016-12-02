491586f7eb001ad2fbe0ef9200bdf71daf303c0e
likaihe <1047561425@qq.com>
2016-12-02 17:51:13 +1300

    understanding

--- DelegatesCallBacks/DelegatesCallBacks/Program.cs
+++ DelegatesCallBacks/DelegatesCallBacks/Program.cs
@@ -6,20 +6,26 @@ using System.Threading.Tasks;
 
 namespace DelegatesCallBacks
 {
-    
+    //Defining a Delegate Type
     delegate void NotifyCalculation(int x, int y, int result);
+
     class Calculator
     {
+        //declaration calcListerner
         NotifyCalculation calcListener;
+
+        //constructor argument that takes an instance of the delegate type
         public Calculator(NotifyCalculation listener)
         {
             calcListener = listener;
         }
+
         public int CalculateProduct(int num1, int num2)
         {
             // perform the calculation
             int result = num1 * num2;
             // notify the delegate that we have performed a calc
+            //the calcListener here acturelly is a print function, nothing more
             calcListener(num1, num2, result);
             // return the result
             return result;
@@ -30,7 +36,7 @@ namespace DelegatesCallBacks
     {
         public static void CalculationPrinter(int x, int y, int result)
         {
-            Console.WriteLine("Calculation Notification: {0} x {1} = {2}",
+            Console.WriteLine("Calculation Notification: {0} + {1} = {2}",
             x, y, result);
         }
     }
